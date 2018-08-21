2aa4736dd9de878fb6e3c2b96d2bf5e5602f9fb0
JulianT2013 <jh_tapping@yahoo.com.au>
2018-08-21 13:04:15 +1000

    Initial commit. Plugin Setup.

---
+++ facebook-footer-link.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin name: Facebook Footer Link
+ * Description: Adds a Facebook profile linke to the end of posts.
+ * Version: 1.0
+ * Author: Julian Tapping
+ */
+
+/* Exit if Accessed Directly */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
---
+++ includes/facebook-footer-link-scripts.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julian
+ * Date: 21/08/2018
+ * Time: 12:57 PM
+ */
